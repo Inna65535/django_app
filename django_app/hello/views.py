034666991bb1f9948ff24febf4bcbfdaf41f08b7
Inna65535 <ppap_16@iCloud.com>
2020-08-23 00:55:09 +0900

    Create django_add

---
+++ django_app/hello/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import Friend
+from .forms import FriendForm
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .forms import FindForm #この文を追記
+from .forms import CheckForm    #☆
+
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'data': data,
+    }
+    return render(request, 'hello/index.html', params)
+
+def create(request):
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
+    return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id':num,
+        'form': FriendForm(instance=obj),
+    }
+    return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id':num,
+        'obj': friend,
+    }
+    return render(request, 'hello/delete.html', params)
+
+class FriendList(ListView):
+    model = Friend
+
+class FriendDetail(DetailView):
+    model = Friend
+
+def find(request):
+    if (request.method == 'POST'):
+        form = FindForm(request.POST)
+        find = request.POST['find']
+        data = Friend.objects.filter(name__icontains=find)
+        msg = 'Result: ' + str(data.count())
+    else:
+        msg = 'search words...'
+        form = FindForm()
+        data =Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'message': msg,
+        'form':form,
+        'data':data,
+    }
+    return render(request, 'hello/find.html', params)
+
+def check(request):
+    params = {
+        'title': 'Hello',
+        'message':'check validation.',
+        'form': CheckForm(),
+    }
+    if (request.method == 'POST'):
+        form = CheckForm(request.POST)
+        params['form'] = form
+        if (form.is_valid()):
+            params['message'] = 'OK!'
+        else:
+            params['message'] = 'no good.'
+    return render(request, 'hello/check.html', params)
